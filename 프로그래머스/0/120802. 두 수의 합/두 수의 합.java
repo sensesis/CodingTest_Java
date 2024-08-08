3c4d601f0c999656b207f454d3b2e0ceeafcbe9a
kittae <39579919+sensesis@users.noreply.github.com>
2024-08-08 20:25:24 +0900

    [level 0] Title: 두 수의 합, Time: 0.03 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/0/120802. 두 수의 합/두 수의 합.java
+++ 프로그래머스/0/120802. 두 수의 합/두 수의 합.java
@@ -1,6 +1,9 @@
-class Solution {
-    public int solution(int num1, int num2) {
-        int answer = -1;
+public class Solution {
+
+    public static int solution(int num1, int num2) {
+
+        int answer = num1 + num2;
+
         return answer;
     }
 }
