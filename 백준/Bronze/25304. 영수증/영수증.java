569db6447101c337877c65d936a43310e217a416
hello_kittae <39579919+sensesis@users.noreply.github.com>
2024-11-11 11:23:39 +0900

    [Bronze IV] Title: 영수증, Time: 104 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.java
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -5,13 +5,13 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        StringTokenizer st;
         int x = Integer.parseInt(br.readLine());
         int n = Integer.parseInt(br.readLine());
         int sum = 0;
         for (int i = 0; i < n; i++) {
 
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            st = new StringTokenizer(br.readLine(), " ");
 
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
