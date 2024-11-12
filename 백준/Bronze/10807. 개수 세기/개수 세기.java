7f633c22dbbf159f212a7bf3034b4f6257776f80
hello_kittae <39579919+sensesis@users.noreply.github.com>
2024-11-12 18:42:31 +0900

    [Bronze V] Title: 개수 세기, Time: 104 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        for (int i = 0; i < arr.length; i++) {
+
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int find = Integer.parseInt(br.readLine());
+
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] == find)
+                count++;
+        }
+
+        System.out.println(count);
+    }
+}
