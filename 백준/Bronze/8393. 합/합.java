4e1b8446a2e253e0448ba4860305441b3594e5d5
hello_kittae <39579919+sensesis@users.noreply.github.com>
2024-11-12 19:19:18 +0900

    [Bronze V] Title: 합, Time: 108 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.java
+++ 백준/Bronze/8393. 합/합.java
@@ -1,17 +1,20 @@
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int sum = 0;
 
-        for (int i = 1; i <= n; i++) {
+         int n = Integer.parseInt(br.readLine());
+         int sum = 0;
+         int[] arr = new int[n];
 
-            sum += i;
-        }
+         for (int i = 0; i < arr.length; i++) {
 
-        System.out.println(sum);
+             arr[i] = i + 1;
+             sum += arr[i];
+         }
+
+         System.out.println(sum);
     }
 }
