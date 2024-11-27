91e90fcb9f6c91ba4cb8b8e9f29782b4ff9c6894
hello_kittae <39579919+sensesis@users.noreply.github.com>
2024-11-27 10:47:26 +0900

    [Bronze I] Title: 세로읽기, Time: 120 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char [][]arr = new char[5][15];
+        String str = "";
+        for (int i =  0; i < 5; i++) {
+
+            str = br.readLine();
+
+            for (int j = 0; j < str.length(); j++) {
+
+                arr[i][j] = str.charAt(j);
+            }
+        }
+
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 5; j++) {
+
+                if (arr[j][i] != '\0')
+                    System.out.print(arr[j][i]);
+            }
+        }
+    }
+}
