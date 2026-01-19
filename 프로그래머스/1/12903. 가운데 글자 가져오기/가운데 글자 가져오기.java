1eabae2e881935ae39b260584c88e4f2a564b251
진기태 <39579919+sensesis@users.noreply.github.com>
2026-01-19 14:59:11 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.02 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        int len = s.length();
+        int mid = len / 2;
+        
+        if (len % 2 == 0) {
+            return s.substring(mid -1, mid + 1);
+        } else {
+            return s.substring(mid, mid + 1);
+        }
+    }
+}
