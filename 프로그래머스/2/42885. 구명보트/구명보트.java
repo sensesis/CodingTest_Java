6d9efcf3cf8adb8751befe56ab2c914083b6b1ba
진기태 <39579919+sensesis@users.noreply.github.com>
2026-01-25 23:10:22 +0900

    [level 2] Title: 구명보트, Time: 11.02 ms, Memory: 58.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        int l = 0;
+        int r = people.length-1;
+        Arrays.sort(people);
+        
+        while (l <= r) {
+            if (people[l] + people[r] <= limit) {
+                l++;
+            }
+            
+             r--;
+             answer++;
+        }
+        
+        return answer;
+    }
+}
