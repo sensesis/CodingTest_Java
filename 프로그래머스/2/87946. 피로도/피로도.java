395e7110a156aeb11c92c809229e143d3520ff58
진기태 <39579919+sensesis@users.noreply.github.com>
2026-02-04 22:18:32 +0900

    [level 2] Title: 피로도, Time: 4.99 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,6 +1,25 @@
+import java.util.*;
+
 class Solution {
+    int maxCount = 0;
+
     public int solution(int k, int[][] dungeons) {
-        int answer = -1;
-        return answer;
+        boolean[] visited = new boolean[dungeons.length];
+        
+        dfs(k, dungeons, visited, 0);
+        
+        return maxCount;
+    }
+    
+    private void dfs(int curK, int[][]dungeons, boolean[] visited, int count) {
+        maxCount = Math.max(maxCount, count);
+        
+        for (int i = 0; i < dungeons.length; i++) {
+            if (!visited[i] && curK >= dungeons[i][0]) {
+                visited[i] = true;
+                dfs(curK - dungeons[i][1], dungeons, visited, count + 1);
+                visited[i] = false;
+            }
+        }
     }
 }
