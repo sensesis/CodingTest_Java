88e7abfb1358c6aab4ce68a52e86b977b70e51ca
진기태 <39579919+sensesis@users.noreply.github.com>
2026-02-05 01:28:00 +0900

    [level 1] Title: 모의고사, Time: 0.59 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] answers) {
+        int[] a = {1,2,3,4,5};
+        int[] b = {2,1,2,3,2,4,2,5};
+        int[] c = {3,3,1,1,2,2,4,4,5,5};
+        int[] scores = new int[3];
+        
+        for (int i = 0; i < answers.length; i++) {
+            if (a[i%a.length] == answers[i]) scores[0]++;
+            if (b[i%b.length] == answers[i]) scores[1]++;
+            if (c[i%c.length] == answers[i]) scores[2]++;
+        }
+ 
+        int maxScore = Math.max(scores[0], Math.max(scores[1], scores[2]));
+        
+        List<Integer> list = new ArrayList<>();
+        
+        for (int i = 0; i < 3; i++) {
+            if (scores[i] == maxScore) {
+                list.add(i+1);
+            }
+        }
+        
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
+    }
+}
