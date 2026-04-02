1fb14ed96733879521fe24bca5b391463af4f657
진기태 <39579919+sensesis@users.noreply.github.com>
2026-04-02 14:47:51 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.78 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,29 +1,24 @@
+import java.util.*;
+
 class Solution {
     public String solution(String s) {
         StringBuilder sb = new StringBuilder();
+        boolean isStart = true;
         
-        // 첫 글자는 무조건 대문자여야 하니까 true로 시작
-        boolean isStartOfWord = true;
-
-        // s.split("") : 한 글자씩 쪼갠다 (공백 포함)
-        for (String cur : s.split("")) {
-            // 1. 현재 글자가 대문자로 변해야 하는 타이밍인가?
-            if (isStartOfWord) {
-                sb.append(cur.toUpperCase());
-            } else {
-                sb.append(cur.toLowerCase());
+        for (String str : s.split("")) {
+            if (isStart) {
+                sb.append(str.toUpperCase());
+                isStart = false;
             }
-
-            // 2. 다음 글자를 위한 판단
-            // 현재 글자가 공백(" ")이라면 -> 다음 글자는 단어의 시작이다! (Flag = true)
-            if (cur.equals(" ")) {
-                isStartOfWord = true;
-            } else {
-                // 공백이 아니라면 -> 다음 글자는 단어의 중간이다. (Flag = false)
-                isStartOfWord = false;
+            else {
+                sb.append(str.toLowerCase());
+            }
+            
+            if (str.equals(" ")) {
+                isStart = true;
             }
         }
-
-        return sb.toString(); // subString 아님
+        
+        return sb.toString();
     }
 }
