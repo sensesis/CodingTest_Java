f5741547815780a13fe1dd2c07a7dc7a17370a88
진기태 <39579919+sensesis@users.noreply.github.com>
2026-04-20 13:57:33 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.07 ms, Memory: 86.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        String str = String.valueOf(n);
+        
+        for (char ch : str.toCharArray()) {
+            answer += Character.getNumericValue(ch);
+        }
+
+        return answer;
+    }
+}
