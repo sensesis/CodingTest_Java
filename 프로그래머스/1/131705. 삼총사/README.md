88843719543e35804514a58932bd3f26d25a9bb3
진기태 <39579919+sensesis@users.noreply.github.com>
2026-09-04 00:19:08 +0900

    [level 1] Title: 삼총사, Time: 0.03 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/README.md
+++ 프로그래머스/1/131705. 삼총사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 삼총사 - 131705 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131705?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131705) 
 
 ### 성능 요약
 
-메모리: 86.3 MB, 시간: 0.03 ms
+메모리: 81.6 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 23일 14:43:27
+2026년 09월 04일 00:19:04
 
 ### 문제 설명
 
--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -1,10 +1,10 @@
 class Solution {
     public int solution(int[] number) {
         int answer = 0;
-
-        for (int i = 0; i < number.length - 2; i++) {
-            for (int j = i + 1; j < number.length - 1; j++) {
-                for (int k = j + 1; k < number.length; k++) {
+        
+        for (int i = 0; i < number.length; i++) {
+            for (int j = i+1; j < number.length; j++) {
+                for (int k = j+1; k < number.length; k++) {
                     if ((number[i] + number[j] + number[k]) == 0) {
                         answer++;
                     }
@@ -15,3 +15,10 @@ class Solution {
         return answer;
     }
 }
+
+/*
+시간복잡도는 3중 for문까지 가능함.
+번호가 같을 수 있음.
+
+1. 이중 for문
+*/
