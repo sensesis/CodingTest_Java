75ddb8fa6fccbc6229c577ff6fcebb79934dc4ff
진기태 <39579919+sensesis@users.noreply.github.com>
2026-09-04 00:36:23 +0900

    [level 1] Title: K번째수, Time: 1.86 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,26 +2,18 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int count = commands.length;
-        int[] answer = new int[count];
+        int[] answer = new int[commands.length];
         
-        for (int i = 0; i < commands.length; i++) {
-            int fir = commands[i][0];
-            int end = commands[i][1];
-            int k =  commands[i][2] - 1;
-            int[] arr = Arrays.copyOfRange(array, fir-1, end);
+        for (int len = 0; len < commands.length; len++) {
+            int i = commands[len][0];
+            int j = commands[len][1];
+            int k = commands[len][2];
             
+            int[] arr = Arrays.copyOfRange(array, i - 1, j);
             Arrays.sort(arr);
-            answer[i] = arr[k];
+            answer[len] = arr[k-1];
         }
         
         return answer;
     }
 }
-
-/*
-제약조건은 따로 제약조건은 없음
-command에서 2, 5, 3 = 2~5번째의 배열에서 3번째 값을 찾아라.
-즉, array는 반복 (while로 깔끔하게 돌리기)
-자르는거는 substring으로 자르기, 그리고 sort한 뒤, 해당 배열 찾기
-*/
